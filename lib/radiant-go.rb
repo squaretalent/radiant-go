07d2de071734018ddce98600010a24c016546f67
Mario Visic <mario.visic@squaretalent.com>
2010-08-15 04:09:52 +0800

    further work. Now has a --force argument. radiant is being generated and gemfile copied

--- lib/radiant-go.rb
+++ lib/radiant-go.rb
@@ -1,12 +1,25 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'radiant-go/installer.rb'
+require File.expand_path(File.dirname(__FILE__)) + '/radiant-go/installer.rb'
 
 
 if ARGV.size == 0
   puts 'please specify a project name' 
 else
-  installer = RadiantGo::Installer.new(ARGV[0])
+  
+  name  = ARGV.shift
+  force = false
+  
+  ARGV.each do |arg|
+    if arg == '--force'
+      force = true 
+    else
+      raise "unknown argument #{arg}"
+    end
+  end
+  
+  installer = RadiantGo::Installer.new(name, force)
   installer.run()
+  
 end
--- lib/radiant-go/installer.rb
+++ lib/radiant-go/installer.rb
@@ -2,9 +2,10 @@ module RadiantGo
 
   class Installer
   
-    def initialize(name)    
-    
-      @project_name = name
+    def initialize(name, force = false)    
+      
+      @project_name  = name
+      @force         = force
       @required_gems =  
       [ { :name => 'radiant', :requirements => '>= 0.9.1'   },
         { :name => 'bundler', :requirements => '>= 0.9.26'  } ]
@@ -14,7 +15,10 @@ module RadiantGo
     def run
     
       if has_required_gems?(@required_gems)
-        generate_radiant_project(@project_name)
+        puts 'generating radiant project'
+        generate_radiant_project(@project_name, @force)
+        puts 'copying gemfile'
+        copy_gemfile(@project_name)
       end
     
     end
@@ -35,8 +39,26 @@ module RadiantGo
     end
 
   
-    def generate_radiant_project(name)
-      %x[radiant #{name}]
+    def generate_radiant_project(name, force)
+      if force
+        %x[radiant #{name} --force]
+      else
+        %x[radiant #{name} --skip]
+      end
+    end
+    
+    def copy_gemfile(name)
+      
+      # we only copy the file if it doesn't exist or if force is on!
+      if !File.exists?(name + '/Gemfile') || @force == true
+        File.new(name + '/Gemfile', File::CREAT) unless File.exists?(name + '/Gemfile')
+
+        source = File.open(File.expand_path(File.dirname(__FILE__)) + '/Gemfile')
+        target = File.open(name + '/Gemfile', 'w')
+
+        target.write( source.read(64) ) while not source.eof?
+      end
+
     end
 
   end

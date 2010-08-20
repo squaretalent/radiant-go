09f0b73e54509dc82c0e8f5b9aa27da129b9b1cc
Mario Visic <mario.visic@squaretalent.com>
2010-08-20 19:58:58 +0800

    Moved the version out to VERSION instead of in the library files

--- lib/radiant-go.rb
+++ lib/radiant-go.rb
@@ -12,19 +12,12 @@ module RadiantGo
 
   class Main
     
-    # Set the release version below
-    
-    @major = '0'
-    @minor = '1'
-    @tiny  = '0'
-    @patch = nil # set to nil for normal release
-    
     @required_gems =  
     [ { :name => 'radiant', :requirements => '>= 0.9.1'   },
       { :name => 'bundler', :requirements => '>= 0.9.26'  } ]
     
     def self.version
-      [@major, @minor, @tiny, @patch].delete_if{|v| v.nil? }.join('.')
+      File.read(File.expand_path(File.dirname(__FILE__)) + '/../VERSION')
     end
     
     def self.required_gems

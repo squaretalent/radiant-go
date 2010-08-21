6a61047c16dd844521d4ab78be045ead71f36107
Mario Visic <mario.visic@squaretalent.com>
2010-08-22 00:45:29 +0800

    Started working on specs, created a few for the Main installer

---
+++ spec/lib/main_spec.rb
@@ -0,0 +1,75 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+module RadiantGo
+
+  module Installers
+
+    describe Main, 'with forcing turned off' do
+    
+      before(:each) do
+        Dir.mkdir 'test'
+        @main = Main.new('test', false)
+      end
+      
+      after(:each) do
+        File.delete 'test/Gemfile'
+        Dir.rmdir 'test'
+      end
+  
+      it 'should create a copy of the gemfile' do
+        @main.copy_gemfile('test')
+        File.exists?('test/Gemfile').should be true
+      end
+      
+      it 'should have a gemfile that isn\'t empty' do
+        @main = Main.new('test', false)
+        @main.copy_gemfile('test')
+        File.zero?('test/Gemfile').should_not be true
+      end
+      
+      it 'should not write over an existing gemfile' do
+        
+        # we create a new gemfile and make it blank
+        gemfile = File.new('test/Gemfile', File::CREAT)
+        gemfile.close
+        File.size('test/Gemfile').should be 0
+        
+        # we run the copy gemfile method, it shouldn't work as the file already exists
+        @main.copy_gemfile('test')
+        File.exists?('test/Gemfile').should be true
+        File.size('test/Gemfile').should be 0
+      end
+      
+    end
+    
+    describe Main, 'with forcing turned on' do
+
+      before(:each) do
+        Dir.mkdir 'test'
+        @main = Main.new('test', true)
+      end
+
+      after(:each) do
+        File.delete 'test/Gemfile'
+        Dir.rmdir 'test'
+      end
+
+      it 'should write over an existing gemfile' do
+        
+        # we create a new gemfile and make it blank
+        gemfile = File.new('test/Gemfile', File::CREAT)
+        gemfile.close
+        File.size('test/Gemfile').should be 0
+        
+        # we copy the file over our gemfile, now it should have a different filesize
+        @main.copy_gemfile('test')
+        File.exists?('test/Gemfile').should be true
+        File.size('test/Gemfile').should be > 0
+      end
+        
+      
+    end
+
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+current_directory = File.dirname(__FILE__) 
+require 'spec/autorun'
+require 'spec'
+require current_directory + '/../lib/radiant-go.rb'
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+Spec::Runner.configure do |config|
+
+  config.mock_with :mocha
+
+end

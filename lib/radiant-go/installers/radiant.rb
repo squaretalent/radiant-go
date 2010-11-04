99146e87d64f41749a55705abf058f71941937fc
Dirk Kelly <dk@dirkkelly.com>
2010-11-04 10:43:12 +0800

    rescue on command to fix the settings failing

--- lib/radiant-go/installers/radiant.rb
+++ lib/radiant-go/installers/radiant.rb
@@ -27,7 +27,13 @@ module RadiantGo
             FileUtils.mkdir_p("db/templates")
             FileUtils.cp(template, "db/templates/#{Config.database_template}")
           end
-          %x[rake db:bootstrap OVERWRITE=true ADMIN_NAME=#{Config.admin_name} ADMIN_USERNAME=#{Config.admin_user} ADMIN_PASSWORD=#{Config.admin_pass} DATABASE_TEMPLATE=#{Config.database_template}]
+          command = "rake db:bootstrap OVERWRITE=true ADMIN_NAME=#{Config.admin_name} ADMIN_USERNAME=#{Config.admin_user} ADMIN_PASSWORD=#{Config.admin_pass} DATABASE_TEMPLATE=#{Config.database_template}"
+          begin
+            %x[#{command}]
+          rescue
+            %x[#{command}]
+          end
+            
         end
       end
       

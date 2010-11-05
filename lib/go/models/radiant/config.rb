2c3e69e0f889a8885ed1d0c63f8389ae3d6c7dd9
Dirk Kelly <dk@dirkkelly.com>
2010-11-05 15:39:22 +0800

    model logic fix

--- lib/go/models/radiant/config.rb
+++ lib/go/models/radiant/config.rb
@@ -5,26 +5,27 @@ module Go
         
         def self.included(base)
           base.class_eval do
-            def self.export(only=nil, except=nil)
+            def self.export(only=nil,except=nil)
+              raise "lol"
               armodels = (ActiveRecord::Base.send(:subclasses) - Page.send(:subclasses)).map { |m| m.name }
               
-              if except && except.present?
-                # Returns only the specified models, unless they're not defined
-                models = except.split(',').map { |m| m.pluralize.classify }
-                models = armodels - models
-              elsif only && only.present?
+              if only && only.present?
                 # Returns all models except those specified
                 models = only.split(',').map { |m| m.pluralize.classify }
                 models = armodels & models
+              elsif except && except.present?
+                # Returns only the specified models, unless they're not defined
+                models = except.split(',').map { |m| m.pluralize.classify }
+                models = armodels - models
               else
                 models = armodels
               end
               
               klasses = models.map { |m| m.constantize }              
               
-              hash = {}
+              hash = { :records => {} }
               klasses.each do |klass|
-                hash[klass.name.pluralize] = klass.find(:all).inject({}) { |h, record| h[record.id.to_i] = record.attributes; h }
+                hash[:records][klass.name.pluralize] = klass.find(:all).inject({}) { |h, record| h[record.id.to_i] = record.attributes; h }
               end
               hash.to_yaml
             end
